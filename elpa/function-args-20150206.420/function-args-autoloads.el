bd6de3b6e5011920c9ee4dc3a80b38f13322147a
W. Nicholas Greene <wng@csail.mit.edu>
2015-05-21 12:34:25 -0400

    Remove old version of helm. Remove function-args.

--- elpa/function-args-20150206.420/function-args-autoloads.el
+++
@@ -1,36 +0,0 @@
-;;; function-args-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (fa-config-default function-args-mode) "function-args"
-;;;;;;  "function-args.el" (21757 2534 391051 927000))
-;;; Generated autoloads from function-args.el
-
-(autoload 'function-args-mode "function-args" "\
-Minor mode for C++ code completion bindings.
-
-\\{function-args-mode-map}
-
-\(fn &optional ARG)" t nil)
-
-(autoload 'fa-config-default "function-args" "\
-Set up default key bindings.
-
-\(fn)" nil nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("function-args-pkg.el") (21757 2534 480571
-;;;;;;  67000))
-
-;;;***
-
-(provide 'function-args-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; function-args-autoloads.el ends here
