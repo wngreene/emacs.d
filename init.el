7915ca4462dadbecec1111b14c8445c9e6d43dc5
W. Nicholas Greene <wng@csail.mit.edu>
2015-05-22 15:00:29 -0400

    Use use-package for git-gutter-fringe.

--- init.el
+++ init.el
@@ -198,6 +198,12 @@
   :config (global-auto-complete-mode t)
   :bind ("<C-tab>" . auto-complete))
 
+;; Git gutter fringe.
+(use-package git-gutter-fringe
+  :ensure t
+  :if window-system
+  :config (global-git-gutter-mode t))
+
 ;; Stuff to run when a window is present.
 (when window-system 
 ;; Set theme.
@@ -221,8 +227,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "#272822" :foreground "#F8F8F2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 85 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
-
-;; Git gutter fringe.
-(require 'git-gutter-fringe)
-(global-git-gutter-mode t)
 )
