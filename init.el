f1957a59cf23b353f9b5916def9ef9409cf79307
W. Nicholas Greene <wng@csail.mit.edu>
2015-09-11 15:41:24 -0400

    Add irony and company.

--- init.el
+++ init.el
@@ -301,11 +301,46 @@
                  (global-semantic-idle-scheduler-mode 1)
                  (semantic-mode 1)))
 
-;; auto-complete.
-(use-package auto-complete
+;; ;; auto-complete.
+;; (use-package auto-complete
+;;   :ensure t
+;;   :config (global-auto-complete-mode t)
+;;   :bind ("<C-tab>" . auto-complete))
+
+;; irony-mode.
+(use-package irony
   :ensure t
-  :config (global-auto-complete-mode t)
-  :bind ("<C-tab>" . auto-complete))
+  :defer t
+  :init
+  (add-hook 'c++-mode-hook 'irony-mode)
+  (add-hook 'c-mode-hook 'irony-mode)
+  (add-hook 'objc-mode-hook 'irony-mode)
+  :config
+  ;; replace the `completion-at-point' and `complete-symbol' bindings in
+  ;; irony-mode's buffers by irony-mode's function
+  (defun my-irony-mode-hook ()
+    (define-key irony-mode-map [remap completion-at-point]
+      'irony-completion-at-point-async)
+    (define-key irony-mode-map [remap complete-symbol]
+      'irony-completion-at-point-async))
+  (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
+
+;; company-mode.
+(use-package company
+  :ensure t
+  :defer t
+  :init (add-hook 'after-init-hook 'global-company-mode)
+  :config
+  (use-package company-irony :ensure t :defer t)
+  (setq company-idle-delay              nil
+        company-minimum-prefix-length   2
+        company-show-numbers            t
+        company-tooltip-limit           20
+        company-dabbrev-downcase        nil
+        company-backends                '((company-irony company-gtags))
+        )
+  :bind ("C-;" . company-complete-common))
 
 ;; Git gutter fringe.
 (use-package git-gutter-fringe
