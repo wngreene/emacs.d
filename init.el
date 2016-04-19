83225c97b4972927dd16eed9a997ce995e947913
W. Nicholas Greene <wng@csail.mit.edu>
2016-04-19 16:45:45 -0400

    Fix lin length for faces.

--- init.el
+++ init.el
@@ -32,7 +32,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background nil :foreground "#F8F8F2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 85 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
+ '(default ((t (:inherit nil :stipple nil :background
+                         nil :foreground "#F8F8F2" :inverse-video nil :box
+                         nil :strike-through nil :overline nil :underline nil :slant
+                         normal :weight normal :height 85 :width
+                         normal :foundry "unknown" :family "DejaVu Sans Mono")))))
 
 ;; Auto refresh.
 (global-auto-revert-mode t)
