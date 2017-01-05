0908605492702247cb5ece7096c520547a5da9ac
rrg <rrg@jakku>
2017-01-04 19:47:30 -0500

    Update font faces.
    Was causing problems on rebuild.

--- init.el
+++ init.el
@@ -32,10 +32,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background
-                         nil :foreground "#F8F8F2" :inverse-video nil :box
-                         nil :strike-through nil :overline nil :underline nil :slant
-                         normal :weight normal :height 85 :width
+ '(default ((t (:inherit nil :stipple
+                         nil :background "#272822" :foreground "#F8F8F2" :inverse-video
+                         nil :box nil :strike-through nil :overline nil :underline
+                         nil :slant normal :weight normal :height 85 :width
                          normal :foundry "unknown" :family "DejaVu Sans Mono")))))
 
 ;; Auto refresh.
@@ -504,5 +504,3 @@
 (use-package langtool
   :ensure t
   :init (setq langtool-language-tool-jar "/home/wng/.local/opt/LanguageTool-3.0/languagetool-commandline.jar"))
-
-;;;
