c8753cec94dc065ef82bd01652a7a732dfce8014
W. Nicholas Greene <wng@csail.mit.edu>
2017-05-31 14:35:53 -0400

    Set image-dired external viewer.

--- init.el
+++ init.el
@@ -24,6 +24,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(image-dired-external-viewer "/usr/bin/feh")
  '(inhibit-startup-screen t)
  '(use-file-dialog nil))
 
