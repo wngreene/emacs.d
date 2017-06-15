2ee24d2259334d7d07bfe2631d8312ba3373bb6f
W. Nicholas Greene <wng@csail.mit.edu>
2017-06-15 17:35:37 -0400

    Switch to geeqie instead of feh.

--- init.el
+++ init.el
@@ -24,7 +24,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(image-dired-external-viewer "/usr/bin/feh")
+ '(image-dired-external-viewer "/usr/bin/geeqie")
  '(inhibit-startup-screen t)
  '(use-file-dialog nil))
 
