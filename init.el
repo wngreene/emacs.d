1bfc11aab429ad14f6b7d51966ec718a392ca713
William Greene <ngreene@sun-vdi-sw-320.corp.zooxlabs.com>
2022-04-28 15:09:05 +0000

    Fix desktop-save so that it can restore window layout in terminal mode.
    From here: https://emacs.stackexchange.com/questions/19190/desktop-save-mode-fails-to-save-window-layout

--- init.el
+++ init.el
@@ -50,6 +50,19 @@
 (global-auto-revert-mode t)
 (setq auto-revert-interval 0.1)
 
+;; desktop-save
+;; Code below allows desktop-save to restore buffer/window layout when in
+;; terminal mode.
+(setq desktop-restore-forces-onscreen nil)
+(add-hook 'desktop-after-read-hook
+          (lambda ()
+            (frameset-restore
+             desktop-saved-frameset
+             :reuse-frames (eq desktop-restore-reuses-frames t)
+             :cleanup-frames (not (eq desktop-restore-reuses-frames 'keep))
+             :force-display desktop-restore-in-current-display
+                 :force-onscreen desktop-restore-forces-onscreen)))
+
 ;; Dired settings.
 (setq delete-by-moving-to-trash t) ;; Move to trash instead of deleting.
 (setq dired-listing-switches "-alh --group-directories-first") ;; Set dired switches.
