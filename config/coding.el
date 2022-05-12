fed9d1f32f50f3676c92f624d8d8c7feba9884d4
W. Nicholas Greene <wnick.greene@strio.ai>
2022-05-12 17:42:05 -0400

    Move misc languages to separate file.

--- config/coding.el
+++ config/coding.el
@@ -48,46 +48,7 @@
          ([f10] . gud-next)
          ([f11] . gud-step)))
 
-;; Bazel.
-(use-package python-mode
-  :mode (("\\.bazel\\'" . python-mode)
-         ("\\.bzl\\'" . python-mode)
-         ("\\BUILD\\'" . python-mode)
-         ("\\WORKSPACE\\'" . python-mode)
-         ))
-
-;; yaml-mode.
-(use-package yaml-mode
-  :ensure t)
-
-;; Markdown mode.
-(use-package markdown-mode
-  :ensure t
-  :mode (("\\.text\\'" . markdown-mode)
-         ("\\.markdown\\'" . markdown-mode)
-         ("\\.md\\'" . markdown-mode)))
-
-;; xml.
-(use-package xml
-  :mode (("\\.launch\\'" . xml-mode)))
-
-;; Docker.
-(use-package dockerfile-mode
-  :ensure t
-  :mode (("Dockerfile\\'" . dockerfile-mode)))
-
-;; Protobuf.
-(use-package protobuf-mode
-  :ensure t
-  :mode (("\\.proto\\'" . protobuf-mode)
-         ("\\.pbtxt\\'" . protobuf-mode)))
-
-;; Typescript.
-(use-package typescript-mode
-  :ensure t
-  :config (progn (setq typescript-indent-level 2))
-  :mode (("\\.ts\\'" . typescript-mode)))
-
 (load-file (concat user-emacs-directory "config/languages/cpp.el"))
 (load-file (concat user-emacs-directory "config/languages/python.el"))
 (load-file (concat user-emacs-directory "config/languages/latex.el"))
+(load-file (concat user-emacs-directory "config/languages/misc.el"))
---
+++ config/languages/misc.el
@@ -0,0 +1,41 @@
+;;; Misc languages.
+
+;; Bazel.
+(use-package bazel-mode
+  :mode (("\\.bazel\\'" . bazel-mode)
+         ("\\.bzl\\'" . bazel-mode)
+         ("\\BUILD\\'" . bazel-mode)
+         ("\\WORKSPACE\\'" . bazel-mode)
+         ))
+
+;; yaml-mode.
+(use-package yaml-mode
+  :ensure t)
+
+;; Markdown mode.
+(use-package markdown-mode
+  :ensure t
+  :mode (("\\.text\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode)
+         ("\\.md\\'" . markdown-mode)))
+
+;; xml.
+(use-package xml
+  :mode (("\\.launch\\'" . xml-mode)))
+
+;; Docker.
+(use-package dockerfile-mode
+  :ensure t
+  :mode (("Dockerfile\\'" . dockerfile-mode)))
+
+;; Protobuf.
+(use-package protobuf-mode
+  :ensure t
+  :mode (("\\.proto\\'" . protobuf-mode)
+         ("\\.pbtxt\\'" . protobuf-mode)))
+
+;; Typescript.
+(use-package typescript-mode
+  :ensure t
+  :config (progn (setq typescript-indent-level 2))
+  :mode (("\\.ts\\'" . typescript-mode)))
