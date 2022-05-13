8a74b1172a62cd4323a0ac3d235810b7afca1150
W. Nicholas Greene <wnick.greene@strio.ai>
2022-05-12 20:58:25 -0400

    Add company.

--- init.el
+++ init.el
@@ -25,6 +25,7 @@
 (load-file (concat user-emacs-directory "config/helm.el"))
 (load-file (concat user-emacs-directory "config/projectile.el"))
 (load-file (concat user-emacs-directory "config/lsp.el"))
+(load-file (concat user-emacs-directory "config/company.el"))
 (load-file (concat user-emacs-directory "config/magit.el"))
 (load-file (concat user-emacs-directory "config/coding.el"))
 
