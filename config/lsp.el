af266cb320667edffde016ac3229935a23b35bab
W. Nicholas Greene <wnick.greene@strio.ai>
2022-05-12 23:24:01 -0400

    Add lsp-ui.

--- config/lsp.el
+++ config/lsp.el
@@ -11,3 +11,14 @@
   (add-hook 'python-mode-hook 'lsp)
   :config
   (lsp-enable-which-key-integration t))
+
+(use-package lsp-ui
+  :ensure t
+  :hook (lsp-mode . lsp-ui-mode)
+  :bind (("C-c h i" . lsp-ui-imenu))
+  :config
+  ;; (setq lsp-ui-sideline-show-hover t)
+  ;; (setq lsp-ui-doc-enable t)
+  ;; :custom
+  ;; (lsp-ui-doc-position 'bottom))
+  )
