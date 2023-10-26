796951a8ab8951cb164cef0691838a0eedc08918
W. Nicholas Greene <ngreene@zoox.com>
2023-10-26 15:50:38 -0700

    Add lsp-ui.

--- config/lsp.el
+++ config/lsp.el
@@ -7,3 +7,14 @@
   (setq lsp-keymap-prefix "C-c ;")  ;; Or 'C-l', 's-l'
   :config
   (lsp-enable-which-key-integration t))
+
+(use-package lsp-ui
+  :ensure t
+  :hook (lsp-mode . lsp-ui-mode)
+  :bind (("C-c h i" . lsp-ui-imenu))
+  :config
+  ;; (setq lsp-ui-sideline-show-hover t)
+  ;; (setq lsp-ui-doc-enable t)
+  ;; :custom
+  ;; (lsp-ui-doc-position 'bottom))
+  )
