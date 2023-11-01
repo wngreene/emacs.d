2f71431fa51b9c26b6de83ce6913952a285f620a
W. Nicholas Greene <ngreene@zoox.com>
2023-11-01 15:18:57 -0700

    Don't use lsp-ui.

--- config/lsp.el
+++ config/lsp.el
@@ -10,14 +10,3 @@
 
 (require 'lsp-diagnostics)
 (lsp-diagnostics-flycheck-enable)
-
-(use-package lsp-ui
-  :ensure t
-  :hook (lsp-mode . lsp-ui-mode)
-  :bind (("C-c h i" . lsp-ui-imenu))
-  :config
-  ;; (setq lsp-ui-sideline-show-hover t)
-  ;; (setq lsp-ui-doc-enable t)
-  ;; :custom
-  ;; (lsp-ui-doc-position 'bottom))
-  )
